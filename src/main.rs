05b2c7e6bef92387d9bcc51cf22dee25f52fecb3
yoshimaru46 <yshr446@gmail.com>
2023-05-31 02:46:25 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ use invaders::{
     render,
 };
 use rusty_audio::Audio;
-use std::{error::Error, io, sync::mpsc, thread, time::Duration};
+use std::{
+    error::Error,
+    io,
+    sync::mpsc,
+    thread,
+    time::{Duration, Instant},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -47,7 +53,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Game Loop
     let mut player = Player::new();
+    let mut instant = Instant::now();
     'gameloop: loop {
+        // Per-frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
         let mut curr_frame = new_frame();
 
         // Input
@@ -56,6 +66,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                 match key_event.code {
                     KeyCode::Left => player.move_left(),
                     KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -64,6 +79,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+        // Updates
+        player.update(delta);
 
         // Draw & render
         player.draw(&mut curr_frame);
