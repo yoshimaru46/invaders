f85b28cdd348737b3e5d9a49ea78224ef11e0447
yoshimaru46 <yshr446@gmail.com>
2023-05-31 01:29:26 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use crossterm::{
+    cursor::{Hide, Show},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
 use rusty_audio::Audio;
-use std::error::Error;
+use std::{error::Error, io};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -11,6 +16,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("win", "win.wav");
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
     Ok(())
 }
